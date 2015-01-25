72b1b62c534471da0335735049b20a6fe1db96b7
petercsengodi <petercsengodi@users.noreply.github.com>
2015-01-25 12:15:33 +0100

    array list map

---
+++ JsonComponentsJava/src/hu/csega/utilities/ArrayListMap.java
@@ -0,0 +1,24 @@
+package hu.csega.utilities;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class ArrayListMap<K, V> extends HashMap<K, ArrayList<V>> {
+
+	/**
+	 * Default serial version uid.
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public List<V> getOrCreate(K key) {
+		ArrayList<V> list = this.get(key);
+		if(list == null) {
+			list = new ArrayList<V>();
+			this.put(key, list);
+		}
+		
+		return list;
+	}
+	
+}
