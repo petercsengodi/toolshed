6599eadefd3dd455215540d97ab0cb4092d394ed
petercsengodi <petercsengodi@users.noreply.github.com>
2017-05-01 12:30:43 +0200

    new util function

--- ToolShedFramework/src/main/java/hu/csega/toolshed/framework/util/GeometricUtil.java
+++ ToolShedFramework/src/main/java/hu/csega/toolshed/framework/util/GeometricUtil.java
@@ -35,4 +35,19 @@ public class GeometricUtil {
 	public static boolean almostEquals(double d1, double d2) {
 		return Math.abs(d1 - d2) < 0.0001;
 	}
+
+	public static boolean inRect(int x, int y, Rectangle rect) {
+		Rectangle r2;
+
+		if(rect.width < 0 || rect.height < 0)
+			r2 = normalizeRectangle(rect);
+		else
+			r2 = rect;
+
+		return inRect(x, y, r2.x, r2.y, r2.x + r2.width, r2.y + r2.height);
+	}
+
+	public static boolean inRect(int x, int y, int x1, int y1, int x2, int y2) {
+		return (x >= x1 && x <= x2 && y >= y1 && y <= y2);
+	}
 }
