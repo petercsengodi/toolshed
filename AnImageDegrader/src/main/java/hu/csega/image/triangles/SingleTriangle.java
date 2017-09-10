f30eedce9eb2f35f1f7c37346530d4ade5a7856d
petercsengodi <petercsengodi@users.noreply.github.com>
2017-09-10 11:03:05 +0200

    adjustment

--- AnImageDegrader/src/main/java/hu/csega/image/triangles/SingleTriangle.java
+++ AnImageDegrader/src/main/java/hu/csega/image/triangles/SingleTriangle.java
@@ -4,7 +4,7 @@ import hu.csega.image.common.BitPipeline;
 
 public class SingleTriangle {
 
-	public static final int SIZE_IN_BYTES = 9;
+	public static final int SIZE_IN_BYTES = 11;
 
 	public int x[] = new int[3];
 	public int y[] = new int[3];
@@ -14,8 +14,8 @@ public class SingleTriangle {
 
 	public void loadFromBytes(byte[] array, int offset, BitPipeline helper) {
 		// x and y coordinates: 10 bits each = 60 bits
-		// r g b colors: 4 bits each = 12 bits
-		// summary: 38 bits = 9 bytes and 0 unused bits (currently)
+		// r g b colors: 8 bits each = 24 bits
+		// summary: 84 bits = 11 bytes and 4 unused bits
 
 		helper.set(array, offset, SIZE_IN_BYTES);
 		x[0] = helper.readBits(10);
@@ -24,9 +24,9 @@ public class SingleTriangle {
 		y[1] = helper.readBits(10);
 		x[2] = helper.readBits(10);
 		y[2] = helper.readBits(10);
-		r = helper.readBits(4) << 4; // to get a value between 0 - 255
-		g = helper.readBits(4) << 4; // to get a value between 0 - 255
-		b = helper.readBits(4) << 4; // to get a value between 0 - 255
+		r = helper.readBits(8); // to get a value between 0 - 255
+		g = helper.readBits(8); // to get a value between 0 - 255
+		b = helper.readBits(8); // to get a value between 0 - 255
 	}
 
 }
