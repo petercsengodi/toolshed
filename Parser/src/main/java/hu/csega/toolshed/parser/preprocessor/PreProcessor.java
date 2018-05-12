7aec4a11c750a92d3247070867553eb98956392a
petercsengodi <petercsengodi@users.noreply.github.com>
2018-05-12 09:38:39 +0200

    parser refactoring

--- Parser/src/main/java/hu/csega/toolshed/parser/preprocessor/PreProcessor.java
+++ Parser/src/main/java/hu/csega/toolshed/parser/preprocessor/PreProcessor.java
@@ -1,10 +1,12 @@
 package hu.csega.toolshed.parser.preprocessor;
 
+import hu.csega.toolshed.parser.preprocessor.helper.UnprocessedText;
 import hu.csega.toolshed.parser.preprocessor.impl.PreProcessorException;
 import hu.csega.toolshed.parser.preprocessor.impl.PreProcessorImpl;
 import hu.csega.units.DefaultImplementation;
 import hu.csega.units.Unit;
 
+import java.io.InputStream;
 import java.util.List;
 
 @Unit
@@ -12,5 +14,7 @@ import java.util.List;
 public interface PreProcessor {
 
 	List<ParserToken> parseText(String source) throws PreProcessorException;
+	List<ParserToken> parseText(InputStream in) throws PreProcessorException;
+	List<ParserToken> parseText(UnprocessedText text) throws PreProcessorException;
 
 }
